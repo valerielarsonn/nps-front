3fe87b46bf3b7319936c67954c7b870f61d0ba80
valerielarsonn <valerielarsonn@gmail.com>
2021-06-26 12:11:33 -0600

    Set up react router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import "milligram";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <Route path="/" component {App}/>
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
